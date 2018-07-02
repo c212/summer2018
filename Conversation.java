20529faa66da6a0d5c4a31fd0d34a4d58ffc544f
c212 <40395648+c212@users.noreply.github.com>
2018-07-02 15:07:19 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,26 +1,12 @@
-// updated and reporting cumulative stats 
+// modified so all data comes from the command 
 
 import java.util.Scanner; 
 
 public class Conversation {
   public static void main(String[] args) {
-    int count = 0; // how many numbers entered thus far
-    double sum = 0; // local variable must be initialized by programmer 
-    System.out.print("Enter value: "); 
-    Scanner input = new Scanner(System.in); 
-    String line = input.nextLine(); 
-    while (! line.equalsIgnoreCase("bye")) { 
-      double number = Double.parseDouble(line); 
-      sum = sum + number; 
-      count++;
-      System.out.println("You typed: " + number); 
-      System.out.println("Average so far: " + ( sum / count )); 
-      System.out.print("Enter value: "); 
-      line = input.nextLine(); 
-    }
-    if (count == 0) 
-      System.out.println("No data."); 
-    else 
-      System.out.println("See you later."); 
+    double[] numbers = new double[args.length];
+    for (int i = 0; i < numbers.length; i++) 
+      System.out.println(args[i] + " will replace numbers[" + i + "] which is currently " + numbers[i] ); 
+      
   }
 }
