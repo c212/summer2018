e0a1e960252767eb095790700d5ceebaccb48a3a
c212 <40395648+c212@users.noreply.github.com>
2018-07-02 15:01:20 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,17 +1,26 @@
-// reading a value that might not be a number
+// updated and reporting cumulative stats 
 
 import java.util.Scanner; 
 
 public class Conversation {
   public static void main(String[] args) {
+    int count = 0; // how many numbers entered thus far
+    double sum = 0; // local variable must be initialized by programmer 
     System.out.print("Enter value: "); 
     Scanner input = new Scanner(System.in); 
     String line = input.nextLine(); 
-    if (line.equalsIgnoreCase("bye")) { 
-      System.out.println("OK, see you later..."); 
-    } else {
+    while (! line.equalsIgnoreCase("bye")) { 
       double number = Double.parseDouble(line); 
+      sum = sum + number; 
+      count++;
       System.out.println("You typed: " + number); 
+      System.out.println("Average so far: " + ( sum / count )); 
+      System.out.print("Enter value: "); 
+      line = input.nextLine(); 
     }
+    if (count == 0) 
+      System.out.println("No data."); 
+    else 
+      System.out.println("See you later."); 
   }
 }
