c18e25b437c914a18e143099eee055e32b89e9c7
c212 <40395648+c212@users.noreply.github.com>
2018-07-02 14:51:38 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,10 +1,17 @@
-// reading one value
+// reading a value that might not be a number
+
+import java.util.Scanner; 
 
 public class Conversation {
   public static void main(String[] args) {
     System.out.print("Enter value: "); 
-    String line = (new java.util.Scanner(System.in)).nextLine(); 
-    double number = Double.parseDouble(line); 
-    System.out.println("You typed: " + number); 
+    Scanner input = new Scanner(System.in); 
+    String line = input.nextLine(); 
+    if (line.equalsIgnoreCase("bye")) { 
+      System.out.println("OK, see you later..."); 
+    } else {
+      double number = Double.parseDouble(line); 
+      System.out.println("You typed: " + number); 
+    }
   }
 }
