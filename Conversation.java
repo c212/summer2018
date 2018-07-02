2b021be1b90d75e6132b3a1c516fed21b273b117
c212 <40395648+c212@users.noreply.github.com>
2018-07-02 15:13:11 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -2,12 +2,34 @@
 
 import java.util.Scanner; 
 
+/*
+
+Welcome to DrJava.  Working directory is C:\Users\dgerman\Desktop\0702-a
+> run Conversation
+No data. 
+> run Conversation 3 2 4 1 1 1
+Average: 2.0 
+> run Conversation 3 2 4 1 1 1 2
+Average: 2.0 
+> run Conversation 3 2 4 1 1 1 2 3
+Average: 2.125 
+> 
+
+
+
+ */
+
 public class Conversation {
   public static void main(String[] args) {
     double[] numbers = new double[args.length];
     for (int i = 0; i < numbers.length; i++) 
       numbers[i] = Double.parseDouble(args[i]); 
-    System.out.println( java.util.Arrays.toString( numbers ) ); 
-    
+    // System.out.println( java.util.Arrays.toString( numbers ) ); 
+    int count = args.length; 
+    double sum = 0; 
+    for (double value : numbers)
+      sum = sum + value; 
+    if (count == 0) System.out.println("No data."); 
+    else System.out.println("Average: " + (sum / count));
   }
 }
