3deb189a39ac4812baf3d3c2d951e65a102151ba
c212 <40395648+c212@users.noreply.github.com>
2018-07-02 15:17:29 -0400

    Add files via upload

--- DataSet.java
+++ DataSet.java
@@ -1,7 +1,14 @@
 public class DataSet {
-  
+  public void add(double value) {
+    System.out.println("Oh, you want to add " + value + " to " + this); 
+  }
   public static void main(String[] args) {
     DataSet a = new DataSet(); 
     System.out.println( a ); 
+    a.add(2.3); 
+    DataSet b = new DataSet(); 
+    b.add(2);
+    b.add(3); 
+    a.add(-1.15); 
   }
 }
