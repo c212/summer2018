da866ff45d00b373adce7d194fb05e710c63ca8d
c212 <40395648+c212@users.noreply.github.com>
2018-07-03 15:07:03 -0400

    my first game
    I am cheating all the way in the name of predictability

--- july03/Game.java
+++ july03/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; 
+
 public class Game {
   private int count; 
   public Game(int initial) {
@@ -8,16 +10,28 @@ public class Game {
   }
   public void extract(int number) {
     this.count -= number;  
+    System.out.println("The pile now: " + this.size()); 
   }
   public static void main(String[] args) {
-    Game a = new Game(34);
-    System.out.println( "From " + a.size() + " I take " + 3 );
-    a.extract(3); 
-    System.out.println( " ... and I am left with " + a.size()); 
-    a.extract(4); 
-    System.out.println( a.size() );
-    a.extract(10); 
-    System.out.println( a.size() );
-    a.extract(2); 
+    Scanner in = new Scanner(System.in); 
+    Game a = new Game((int)(Math.random() * 50 + 50)); 
+    System.out.println("Game starts with " + a.size() + " marbles."); 
+    do { 
+      System.out.print("You move first: "); 
+      int human = Integer.parseInt(in.nextLine()); 
+      a.extract(human); 
+      if (a.size() < 0) {
+        System.out.println("You lost.");  
+        break; 
+      }
+      int computer = 1; 
+      System.out.println("Computer takes " + computer + " marbles."); 
+      a.extract(computer); 
+      if (a.size() < 0) {
+        System.out.println("Computer lost.");  
+        break; 
+      }
+    } while (true); 
+    
   }
 }
