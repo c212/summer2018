38bf5744056c51cd293b6ca2468941c8a23cce1c
c212 <40395648+c212@users.noreply.github.com>
2018-07-03 14:48:18 -0400

    designed a pile class
    the pile knows it size

--- july03/Game.java
+++ july03/Game.java
@@ -1,3 +1,13 @@
 public class Game {
-  
+  private int count; 
+  public Game(int initial) {
+    this.count = initial; 
+  }
+  public int size() {
+    return this.count;  
+  }
+  public static void main(String[] args) {
+    Game a = new Game(34);
+    System.out.println( a.size() );
+  }
 }
