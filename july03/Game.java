b2a53357ca35d4a2b960ad246cdcdb07bc0743e2
c212 <40395648+c212@users.noreply.github.com>
2018-07-03 14:54:38 -0400

    added extract
    dfjkhgdfkjhgdf

--- july03/Game.java
+++ july03/Game.java
@@ -6,8 +6,18 @@ public class Game {
   public int size() {
     return this.count;  
   }
+  public void extract(int number) {
+    this.count -= number;  
+  }
   public static void main(String[] args) {
     Game a = new Game(34);
+    System.out.println( "From " + a.size() + " I take " + 3 );
+    a.extract(3); 
+    System.out.println( " ... and I am left with " + a.size()); 
+    a.extract(4); 
     System.out.println( a.size() );
+    a.extract(10); 
+    System.out.println( a.size() );
+    a.extract(2); 
   }
 }
